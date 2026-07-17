ab70d4446a961d566390968b45e6e81c3589b3d4
Swadesh <mintdonut118@gmail.com>
2026-07-17 13:43:48 +0530

    Day 1: exercises 1-3 done

--- day1.py
+++ day1.py
@@ -4,11 +4,16 @@
 # for word in words:
 #    result=result+word[0].upper()+"."
 # print(result)
-name=input("enter the name to reverse")
-result=name[::-1]
-print(result)
-if name==result:
- print("palindrom")
-else:
-   print("not palindrom")
-       
+# name=input("enter the name to reverse")
+# result=name[::-1]
+# print(result)
+# if name==result:
+#  print("palindrom")
+# else:
+#    print("not palindrom")
+name=input("enter the vowel check: ")
+count=0
+for letter in name:
+    if letter in "aeiou":
+     count=count+1
+print(f"vowel count:{count}")
