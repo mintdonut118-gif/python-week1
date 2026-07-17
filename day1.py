8b5f243fcc8b90746903c38d606ab47221050262
Swadesh <mintdonut118@gmail.com>
2026-07-17 13:30:45 +0530

    Day 1 Exercise 2: reverse string + palindrome check

--- day1.py
+++ day1.py
@@ -1,6 +1,14 @@
-name=input("enter your name: ")
-words=name.split()
-result=""
-for word in words:
-    result=result+word[0].upper()+"."
+# name=input("enter your name: ")
+# words=name.split()
+# result=""
+# for word in words:
+#    result=result+word[0].upper()+"."
+# print(result)
+name=input("enter the name to reverse")
+result=name[::-1]
 print(result)
+if name==result:
+ print("palindrom")
+else:
+   print("not palindrom")
+       
