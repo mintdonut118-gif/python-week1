0551105544c4bef8bc1a536a66489d345cd6ca9e
Swadesh <mintdonut118@gmail.com>
2026-07-18 12:19:30 +0530

    Day 1 complete: all 4 exercises done

--- day1.py
+++ day1.py
@@ -11,9 +11,16 @@
 #  print("palindrom")
 # else:
 #    print("not palindrom")
-name=input("enter the vowel check: ")
-count=0
-for letter in name:
-    if letter in "aeiou":
-     count=count+1
-print(f"vowel count:{count}")
+# name=input("enter the vowel check: ")
+# count=0
+# for letter in name:
+#     if letter in "aeiou":
+#      count=count+1
+# print(f"vowel count:{count}")
+price=input("enter the price")
+price=price.replace("₹","")
+price=price.replace(",","")
+price=price.strip()
+price=int(price)
+gst=price * 1.18
+print(f"base price{price} and new gst price{ gst:.2f}")
