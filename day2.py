a5023d4a8113390ed236e21fdf38431d3980a606
Swadesh <mintdonut118@gmail.com>
2026-07-18 13:06:58 +0530

    Day 2: countdown with break

--- day2.py
+++ day2.py
@@ -1,10 +1,18 @@
-for number in range (1 ,51):
+# for number in range (1 ,51):
 
-  if  number%5==0 and number%3==0:
-    print("fizzbuzz")
-  elif number%5==0:
-    print("buzz")
-  elif number%3==0:
-    print("fizz")
-  else :
-    print(number)
+#   if  number%5==0 and number%3==0:
+#     print("fizzbuzz")
+#   elif number%5==0:
+#     print("buzz")
+#   elif number%3==0:
+#     print("fizz")
+#   else :
+#     print(number)
+number=10
+number=int(number)
+while number>0:
+ print(number)
+ number=number-1
+ if number==4:
+    break
+ 
