6108a342175fe8c569491a141000f06a6a173569
Swadesh <mintdonut118@gmail.com>
2026-07-18 12:56:14 +0530

    Day 2: FizzBuzz complete

---
+++ day2.py
@@ -0,0 +1,10 @@
+for number in range (1 ,51):
+
+  if  number%5==0 and number%3==0:
+    print("fizzbuzz")
+  elif number%5==0:
+    print("buzz")
+  elif number%3==0:
+    print("fizz")
+  else :
+    print(number)
