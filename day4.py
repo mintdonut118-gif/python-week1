1748888f5c31abbe439b8caeed9aa7fb2c23fc8b
Swadesh <mintdonut118@gmail.com>
2026-07-21 20:08:20 +0530

    day4: min/max by hand with champion loop

--- day4.py
+++ day4.py
@@ -1,27 +1,40 @@
 
-todo=[]
-while True:
-    print("1.add task 2.remove task 3.view task 4.quit")
-    choice=input("enter the choice:")
-    choice=int(choice)
-    if choice==1:
-         task=input("enter the task")
-         todo.append(task)    
-    elif choice==3:
+# todo=[]
+# while True:
+#     print("1.add task 2.remove task 3.view task 4.quit")
+#     choice=input("enter the choice:")
+#     choice=int(choice)
+#     if choice==1:
+#          task=input("enter the task")
+#          todo.append(task)    
+#     elif choice==3:
        
-     for i ,item in enumerate(todo,start=1):
-          print(f"Task{i} {item}")
-    elif choice==2:
-      remove=input("which task to remove")
-      if remove in todo:
-        todo.remove(remove)
-      else:
-         print("taska not found")
+#      for i ,item in enumerate(todo,start=1):
+#           print(f"Task{i} {item}")
+#     elif choice==2:
+#       remove=input("which task to remove")
+#       if remove in todo:
+#         todo.remove(remove)
+#       else:
+#          print("task not found")
 
 
-    elif choice==4:
-        print("quit")
-        break
+#     elif choice==4:
+#         print("quit")
+#         break
+prices = [450, 1200, 99, 780, 310]
+smallest=prices[0]
+for p in prices:
+    if p<smallest:
+        smallest=p
+        print(f"{smallest}")
+largest=prices[0]
+for p in prices:
+    if p>largest:
+        largest=p
+        print(f"{largest}")
+
+
 
         
     
