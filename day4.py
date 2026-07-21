524d030882a3692e3bb9a4adfce40256ad4d729d
Swadesh <mintdonut118@gmail.com>
2026-07-21 20:21:22 +0530

    day 4 completed

--- day4.py
+++ day4.py
@@ -22,17 +22,28 @@
 #     elif choice==4:
 #         print("quit")
 #         break
-prices = [450, 1200, 99, 780, 310]
-smallest=prices[0]
-for p in prices:
-    if p<smallest:
-        smallest=p
-        print(f"{smallest}")
-largest=prices[0]
-for p in prices:
-    if p>largest:
-        largest=p
-        print(f"{largest}")
+# prices = [450, 1200, 99, 780, 310]
+# smallest=prices[0]
+# for p in prices:
+#     if p<smallest:
+#         smallest=p
+#         print(f"{smallest}")
+# largest=prices[0]
+# for p in prices:
+#     if p>largest:
+#         largest=p
+#         print(f"{largest}")
+# routes = [("Hyderabad", "Bangalore", 570), ("Hyderabad", "Vijayawada", 270)]
+# for intial,destination,distance in routes:
+#     print(f"{intial}--->{destination},{distance}")
+nums = [1, 3, 3, 7, 1, 9, 7]
+duplicate=[]
+for i in nums:
+    if i not in duplicate:
+        duplicate.append(i)
+print(duplicate)
+
+
 
 
 
