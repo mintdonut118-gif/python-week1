8531d7aacb8caaaf10e664c769ecea220195e6df
Swadesh <mintdonut118@gmail.com>
2026-07-24 11:14:36 +0530

    day5: fare lookup + word counter

--- day5.py
+++ day5.py
@@ -1,7 +1,17 @@
-fares={"Bangalore":950,"Chennai":500,"Hyderbad":788}
-print(fares["Chennai"])
-destination=input("enter the destination:")
-if destination in fares:
-    print(f"fare to destination{destination}is: {fares[destination]}")
-else :
-    print("Destination not found")
+# fares={"Bangalore":950,"Chennai":500,"Hyderbad":788}
+# print(fares["Chennai"])
+# destination=input("enter the destination:")
+# if destination in fares:
+#     print(f"fare to destination{destination}is: {fares[destination]}")
+# else :
+#     print("Destination not found")
+sentence="the bus has left the depot"
+words=sentence.split()
+count={}
+for word in words:
+    if word in count:
+        count[word]=count[word]+1
+    else:
+        count[word]=1
+
+print(count)
