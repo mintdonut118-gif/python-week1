36b9046234199ff4dca8a5914741d27baa2b5d6b
Swadesh <mintdonut118@gmail.com>
2026-07-24 11:53:12 +0530

    day5: route report

--- day5.py
+++ day5.py
@@ -15,26 +15,30 @@
 #         count[word]=1
 
 # print(count)
-todo=[]
-while True:
-    choice=input("1.add 2.view")
-    choice=int(choice)
-    if choice==1:
-        item=input("enter the item")
-        todo.append({"task": item, "done": False})
-    elif choice==2:
-        for i,item in enumerate(todo,start=1):
-            mark = "x" if item["done"] else " "
-            print(f"Task {i}: [{mark}] {item['task']}")
-    elif choice==3:
-        done=input("which task is done")
-        done=int(done)
-        n=int(done)-1
-        todo[n]["done"]=True
-    elif choice == 4:
-     remove_num= input("which task to remove: ")
-     remove_num = int(remove_num)
-     n = remove_num - 1
-     todo.pop(n)
-
-           
+# todo=[]
+# while True:
+#     choice=input("1.add 2.view")
+#     choice=int(choice)
+#     if choice==1:
+#         item=input("enter the item")
+#         todo.append({"task": item, "done": False})
+#     elif choice==2:
+#         for i,item in enumerate(todo,start=1):
+#             mark = "x" if item["done"] else " "
+#             print(f"Task {i}: [{mark}] {item['task']}")
+#     elif choice==3:
+#         done=input("which task is done")
+#         done=int(done)
+#         n=int(done)-1
+#         todo[n]["done"]=True
+#     elif choice == 4:
+#      remove_num= input("which task to remove: ")
+#      remove_num = int(remove_num)
+#      n = remove_num - 1
+#      todo.pop(n)
+routes = [
+    {"from": "Hyderabad", "to": "Bangalore", "fare": 950},
+    {"from": "Hyderabad", "to": "Vijayawada", "fare": 550},
+]
+for r in routes:
+    print(f"{r['from']} -> {r['to']}: {r['fare']}")
